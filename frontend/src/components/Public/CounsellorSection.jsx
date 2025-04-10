3fb77fc2c6e713d479dd7108a16a72897955b6d4
harry247302 <shadab.shaikh.tech@gmail.com>
2025-04-10 02:27:43 -0700

    counselor

--- frontend/src/components/Public/CounsellorSection.jsx
+++ frontend/src/components/Public/CounsellorSection.jsx
@@ -53,11 +53,12 @@ function CounsellorSection() {
 
         {/* Gallery Component */}
         <motion.div
+       
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.7, delay: 0.4 }}
-          className="mt-10 w-full"
+          className="mt-10 w-full Counselor"
         >
           <Gallery isLoading={isLoading} testimonial={testimonial} />
         </motion.div>
