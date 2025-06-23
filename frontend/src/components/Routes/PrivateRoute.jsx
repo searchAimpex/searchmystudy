ab8746cd2f06dd6cb5c2b15fba003f28c7a2a5b6
searchAimpex <searchmystudy@gmail.com>
2025-06-23 22:58:49 +0530

    directory

--- frontend/src/components/Routes/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import AdminLayout from '../Layouts/AdminLayout';
-
-const PrivateRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo?.role === "admin" ? <AdminLayout /> : <Navigate to='/login' replace />
-};
-export default PrivateRoute;
